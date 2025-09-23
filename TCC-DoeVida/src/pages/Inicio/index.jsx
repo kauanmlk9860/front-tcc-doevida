071df09ea43390dd53ac45717c1fd543d9ca307b
Kauan Rodrigues <kauan.leonel.rodrigues@gmail.com>
2025-09-23 17:21:15 -0300

    ajustes no index.jsx e inicio do style.css da tela de cadastro de usuario/doador

--- TCC-DoeVida/src/pages/Inicio/index.jsx
+++ TCC-DoeVida/src/pages/Inicio/index.jsx
@@ -4,21 +4,18 @@ import Logo_Branca from '../../assets/Logo_Branca.png'
 function Inicio() {
 
   return (
-      <div className='container'>
-        <form>
-          <img src={Logo_Branca}/>
-          <h1>DOEVIDA</h1>
-          <input name='Nome completo' type="text" />
-          <input name='E-mail' type="email" />
-          <input name='Digite sua Senha' type="text" />
-          <input name='Confirme sua Senha' type="text" />
-          <button type='button'>Criar Conta</button>
-          <button type='button'>
-            Já tem uma conta?
-            Faça Login
-          </button>
-        </form>
-      </div>
+    <div className='container'>
+      <img src={Logo_Branca} />
+      <form id='formulario_doador'>
+        <h1>DOEVIDA</h1>
+        <input id='name_doador' name='Nome completo' type="text" />
+        <input id='email_doador' name='E-mail' type="email" />
+        <input id='senha_doador' name='Digite sua Senha' type="text" />
+        <input id='confirmar_doador' name='Confirme sua Senha' type="text" />
+        <button id='criar_doador' type='button'>Criar Conta</button>
+        <button id='tem_conta' type='button'>Já tem uma conta?</button>
+      </form>
+    </div>
   )
 }
 
