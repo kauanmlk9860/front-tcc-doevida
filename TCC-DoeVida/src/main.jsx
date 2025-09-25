8f98912353b2efc5efb564654a0da1ef4c0ea31a
Kauan Rodrigues <kauan.leonel.rodrigues@gmail.com>
2025-09-25 15:08:26 -0300

    tela de recuperação feita

--- TCC-DoeVida/src/main.jsx
+++ TCC-DoeVida/src/main.jsx
@@ -2,10 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Login from './pages/Login'
 import Cadastro from './pages/Cadastro'
+import Redefinir_senha from './pages/Redefinir_senha'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Cadastro />
+    <Redefinir_senha />
   </StrictMode>,
 )
