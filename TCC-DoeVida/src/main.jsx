15ca77313e78ccb789dfa39d000d52dca955d15a
GABRIEL DA SILVA SOARES <24122283@CFP127ALUNO.LOCAL>
2025-09-25 16:22:07 -0300

    ajustes

--- TCC-DoeVida/src/main.jsx
+++ TCC-DoeVida/src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import Login from './pages/Login'
 import Cadastro from './pages/Cadastro'
+import Login from './pages/Login'
 import Redefinir_senha from './pages/Redefinir_senha'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Redefinir_senha />
+    <Login />
   </StrictMode>,
 )
