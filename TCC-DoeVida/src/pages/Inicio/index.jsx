c48830fd28e5bb37fe61fd8a9198d73f409c37b3
GABRIEL DA SILVA SOARES <24122283@dsn127m1246727.cfp127aluno.local>
2025-09-25 09:50:21 -0300

    adicao dos placeholders nos inputs, estilizacao desses e da bola no canto superior esquerdo

--- TCC-DoeVida/src/pages/Inicio/index.jsx
+++ TCC-DoeVida/src/pages/Inicio/index.jsx
@@ -4,14 +4,16 @@ import Logo_Branca from '../../assets/Logo_Branca.png'
 function Inicio() {
 
   return (
+  
     <div className='container'>
+      <div class="bola-cortada"></div>
       <img src={Logo_Branca} />
       <form id='formulario_doador'>
         <h1>DOEVIDA</h1>
-        <input id='name_doador' name='Nome completo' type="text" />
-        <input id='email_doador' name='E-mail' type="email" />
-        <input id='senha_doador' name='Digite sua Senha' type="text" />
-        <input id='confirmar_doador' name='Confirme sua Senha' type="text" />
+        <input id='name_doador' placeholder='Nome Completo' name='Nome completo' type="text" />
+        <input id='email_doador' placeholder='E-mail' name='E-mail' type="email" />
+        <input id='senha_doador' placeholder='Senha' name='Digite sua Senha' type="text" />
+        <input id='confirmar_doador' placeholder='Confirme sua Senha' name='Confirme sua Senha' type="text" />
         <button id='criar_doador' type='button'>Criar Conta</button>
         <button id='tem_conta' type='button'>Já tem uma conta?</button>
       </form>
