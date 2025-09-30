194d1b1dc377b92c2321d45021448e6e9e38d76b
KAUAN DE ASSIS LEONEL RODRIGUES <24122535@dsn127m1241958.cfp127aluno.local>
2025-09-30 12:24:05 -0300

    Ajustes 2.0

--- TCC-DoeVida/src/main.jsx
+++ TCC-DoeVida/src/main.jsx
@@ -3,7 +3,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Cadastro from './pages/Cadastro'
 import Login from './pages/Login'
-import RecuperarSenha from './pages/Recuperar_senha'
+import Recuperar_senha from './pages/Recuperar_senha'
+import Redefinir_senha from './pages/Redefinir_senha'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
@@ -13,7 +14,8 @@ createRoot(document.getElementById('root')).render(
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<Login />} />
         <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/recuperar-senha" element={<RecuperarSenha />} />
+        <Route path="/recuperar-senha" element={<Recuperar_senha />} />
+        <Route path="/redefinir-senha" element={<Redefinir_senha />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
