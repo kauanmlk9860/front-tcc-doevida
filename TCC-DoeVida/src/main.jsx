4e0505433bc68351872c146d70ea955d8c647d4f
GABRIEL DA SILVA SOARES <24122283@dsn127m1246727.cfp127aluno.local>
2025-09-30 15:31:01 -0300

    git pull

--- TCC-DoeVida/src/main.jsx
+++ TCC-DoeVida/src/main.jsx
@@ -3,13 +3,10 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import Cadastro from './pages/Cadastro'
 import Login from './pages/Login'
-<<<<<<< HEAD
 import RecuperarSenha from './pages/Recuperar_senha'
 import SaibaMais from './pages/Saiba_mais'
-=======
 import Recuperar_senha from './pages/Recuperar_senha'
 import Redefinir_senha from './pages/Redefinir_senha'
->>>>>>> 194d1b1dc377b92c2321d45021448e6e9e38d76b
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
