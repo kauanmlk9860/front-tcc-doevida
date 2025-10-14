b210c0d830daf3c232659f811fb1b881f09838d9
GABRIEL DA SILVA SOARES <24122283@dsn127m1241959.cfp127aluno.local>
2025-10-14 12:08:57 -0300

    adicao dos arquivos de recuperar senha do Hospital

--- TCC-DoeVida/src/main.jsx
+++ TCC-DoeVida/src/main.jsx
@@ -1,16 +1,17 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-import Cadastro from './pages/Cadastro';
-import Login from './pages/Login';
-import SaibaMais from './pages/Saiba_mais';
-import Recuperar_senha from './pages/Recuperar_senha';
-import Redefinir_senha from './pages/Redefinir_senha';
-import Home from './pages/Home';
-import Hospital_Login from './pages/Hospital_Login';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Cadastro from "./pages/Cadastro";
+import Login from "./pages/Login";
+import SaibaMais from "./pages/Saiba_mais";
+import Recuperar_senha from "./pages/Recuperar_senha";
+import Redefinir_senha from "./pages/Redefinir_senha";
+import Home from "./pages/Home";
+import Hospital_Login from "./pages/Hospital_login";
+import Hospital_recuperar_senha from "./pages/Recuperar_senha_hospital";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
@@ -21,7 +22,8 @@ createRoot(document.getElementById('root')).render(
         <Route path="/redefinir-senha" element={<Redefinir_senha />} />
         <Route path="/saiba-mais" element={<SaibaMais />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/hospital-login" element={<Hospital_Login />} /> 
+        <Route path="/hospital-login" element={<Hospital_Login />} />
+        <Route path="/hospital-recuperar-senha" element={<Hospital_recuperar_senha />} />
         <Route path="*" element={<Navigate to="/login" replace />} />
       </Routes>
     </BrowserRouter>
