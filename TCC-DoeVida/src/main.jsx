860873cb6cd392d946f080ba63635592c3d292f3
GABRIEL DA SILVA SOARES <24122283@dsn127m1241959.cfp127aluno.local>
2025-10-14 10:00:17 -0300

    ajuste de card

--- TCC-DoeVida/src/main.jsx
+++ TCC-DoeVida/src/main.jsx
@@ -1,35 +1,3 @@
-<<<<<<< HEAD
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import Cadastro from './pages/Cadastro'
-import Login from './pages/Login'
-import SaibaMais from './pages/Saiba_mais'
-import Recuperar_senha from './pages/Recuperar_senha'
-import Redefinir_senha from './pages/Redefinir_senha'
-import Home from './pages/Home'
-import Hospital_cadastro from './pages/Hospital_cadastro'
-import './index.css'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Hospital_cadastro/>
-  </StrictMode>
-)
-
-//<BrowserRouter>
-// //<Routes>
-//   //<Route path="/" element={<Navigate to="/home" replace />} />
-//   //<Route path="/login" element={<Login />} />
-//   // <Route path="/cadastro" element={<Cadastro />} />
-//   <Route path="/recuperar-senha" element={<Recuperar_senha />} />
-//   <Route path="/redefinir-senha" element={<Redefinir_senha />} />
-//   <Route path='/saiba-mais' element={<SaibaMais />} />
-//   <Route path='/home' element={<Home />} />
-//   <Route path="*" element={<Navigate to="/login" replace />} />
-// </Routes>
-// </BrowserRouter>
-=======
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
@@ -59,4 +27,3 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </StrictMode>
 );
->>>>>>> cfcfd9f05734f2356414c11533d9cf025e36a8d5
